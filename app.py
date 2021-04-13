c63c15c2a55b34e665fa3d412bfd368906e18e73
eri <contact.erionline@gmail.com>
2021-04-13 12:22:14 +0200

    fix: Typo sur nom de route

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def liste_dossiers_patients():
 @app.route('/dossiers/nouveau/')
 def nouveau_dossier_patient():
     """Crée un nouveau dossier pour un patient"""
-    return render_template("views/ajout_dossiers.html")
+    return render_template("views/ajout_dossier.html")
 
 @app.route('/dossier/')
 def dossier_patient():
