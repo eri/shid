27f18297fdc6d1a60781c4a51200bfc872d4c548
eri <contact.erionline@gmail.com>
2021-04-14 15:37:46 +0200

    new: Extension pour communication API avec Mongo

---
+++ extensions/mongo.py
@@ -0,0 +1,15 @@
+import pymongo
+
+mongo = pymongo.MongoClient("localhost", 27017, serverSelectionTimeoutMS=100)
+
+def find(db, collection, query):
+    query = mongo[db][collection].find(query)
+    if query and query.count() > 0:
+        return query
+    return None
+
+def insert_one(db, collection, data):
+    query = mongo[db][collection].insert_one(data)
+    if query and query.count() > 0:
+        return query
+    return None
