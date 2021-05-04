8f656fe9583385c81bee314595cd3b554b743747
eri <contact.erionline@gmail.com>
2021-05-04 13:54:49 +0200

    fix: Revert changes (Docker tests)

--- app.py
+++ app.py
@@ -211,5 +211,5 @@ def search_db(type, query):
     return search
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True, host="0.0.0.0", port="5000")
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5000")
