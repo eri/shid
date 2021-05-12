a167845854bcd0bfacbd7ae3a0389c9e6dae2752
eri <contact.erionline@gmail.com>
2021-05-12 06:24:59 +0200

    new: Add new Mongo function to sort results

--- app/extensions/mongo.py
+++ app/extensions/mongo.py
@@ -5,11 +5,17 @@ mongo = pymongo.MongoClient("localhost", 27017, serverSelectionTimeoutMS=100)
 def find(db, collection, query):
     query = mongo[db][collection].find(query)
     if query and query.count() > 0:
-        return query[0]
+        return query
+    return None
+
+def find_sorted(db, collection, query, sorter):
+    query = mongo[db][collection].find(query).sort(sorter['k'], sorter['o'])
+    if query and query.count() > 0:
+        return query
     return None
 
 def insert_one(db, collection, data):
     query = mongo[db][collection].insert_one(data)
-    if query and query.count() > 0:
+    if query:
         return query
     return None
